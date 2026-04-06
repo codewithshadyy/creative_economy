85861761a1a1143ae5d57d4458234b7c2f98c5b6
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-04-06 18:59:06 +0300

    commented caching with Redis then use local memory for caching.

--- config/settings.py
+++ config/settings.py
@@ -175,12 +175,19 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
 # caching settings
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": "",
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#         }
+#     }
+# }
+
 CACHES = {
     "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "LOCATION": "unique-snowflake",
     }
 }
