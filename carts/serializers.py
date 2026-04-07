05beb7905ae705ae8971a956e7aaccf91f874f18
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-04-07 12:01:21 +0300

    Cart app serializers for CartItem and Cart Models.

---
+++ carts/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Cart, CartItem
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product_name = serializers.CharField(source="product.name", read_only=True)
+    price = serializers.DecimalField(source="product.price", decimal_places=2, max_digits=10, read_only=True)
+
+    class Meta:
+        model = CartItem
+        fields = ["id", "product", "product_name", "quantity", "price"]
+
+
+class CartSerializer(serializers.ModelSerializer):
+    items = CartItemSerializer(many=True)
+
+    class Meta:
+        model = Cart
+        fields = ["id", "user", "items"]
