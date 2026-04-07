5e3aa5c019baff268b496fad5328b0fd70f423ce
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-04-07 12:09:21 +0300

    Created CartViewSet using viewsets

--- carts/views.py
+++ carts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from .serializers import CartSerializer, CartItemSerializer
+from .models import Cart, CartItem
 
-# Create your views here.
+class CartViewSet(viewsets.ModelViewSet):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        return Cart.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        return serializer.save(user=self.request.user)
