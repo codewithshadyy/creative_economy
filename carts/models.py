1d36f9f72d6a92687d45f2b6167ff8068c475de7
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-04-07 11:52:44 +0300

    created carts app models:Cart and CartItem

--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.conf import settings
+from products.models import Product
+User = settings.AUTH_USER_MODEL
 
-# Create your models here.
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="cart")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Cart-{self.user}"
+    
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    class Meta:
+        unique_together = ["cart", "product"]
+
+    def __str__(self):
+        return f"{self.product.name} ({self.product})"        
