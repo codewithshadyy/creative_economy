1ad75cfcf50b901a2495114c806a041df3d440f0
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-04-08 18:33:53 +0300

    Building the checkout system to handle orders

--- orders/models.py
+++ orders/models.py
@@ -1,2 +1,33 @@
 from django.db import models
+from carts.models import Cart
+from products.models import Product
+from django.conf import settings
+User = settings.AUTH_USER_MODEL
 
+
+class Order(models.Model):
+    STATUS_CHOICES = (
+        ("pending", "Pending"),
+        ("paid", "Paid"),
+        ("shipped", "Shipped"),
+        ("delivered", "Delivered"),
+        ("cancelled", "Cancelled"),
+        )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(choices=STATUS_CHOICES, default="pending")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"order {self.id}-{self.user}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Order {self.order}- {self.product}"
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework import response
+from .serializers import OrderItemSerializer, OrderSerializer
+from .models import Order, OrderItem
+from django.conf import settings
+User = settings.AUTH_USER_MODEL
